2c49123ade5940ad8bde68fa10f13cb3d7ba4120
netlord2022 <netlord2022@gmail.com>
2025-12-26 20:12:32 +0100

    fix

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,11 @@ describe("template spec", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173/")
   })
+
   it("front page can be opened", () => {
     cy.get("h1").contains("الحلول المتكاملة لتجهيزات الفنادق والمطاعم")
   })
+
   it("products page can be opened", () => {
     cy.get('[data-sy="products"]').click()
     cy.url().should("include", "/products")
